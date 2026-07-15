421f82043d1510943bfe4b973951af374b68f916
Mikhail Ryabchikov <Nolifegonnamad@gmail.com>
2026-07-15 13:31:27 +0400

    meeting 1 finished

--- core/src/main/java/com/mygdsx/game/MyGdxGame.java
+++ core/src/main/java/com/mygdsx/game/MyGdxGame.java
@@ -8,22 +8,32 @@ import com.badlogic.gdx.utils.ScreenUtils;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MyGdxGame extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture birdTexture;
+
+    int birdX = 0;
+    int birdY = 0;
+    int birdSpeed = 5;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        birdTexture = new Texture("bird0.png");
     }
 
     @Override
     public void render() {
+        birdX += birdSpeed;
+        birdY += birdSpeed;
+
         ScreenUtils.clear(1, 0, 0, 1);
         batch.begin();
+        batch.draw(birdTexture, birdX, birdY);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
+        birdTexture.dispose();
     }
 }
